84ff0356ad25b1d96432408b176f18edd0cdbd2f
Fritz Meissner <fritz.meissner@gmail.com>
2018-02-21 01:19:39 +0200

    added some helpful config

--- config/config.rb
+++ config/config.rb
@@ -7,6 +7,54 @@ module Gilded
       @defaults = YAML.load_file('config/gilded_config.yml')['defaults']
     end
 
+    def url
+      @defaults['url']
+    end
+
+    def protocol
+      @defaults['protocol']
+    end
+
+    def host
+      @defaults['host']
+    end
+
+    def port
+      @defaults['port']
+    end
+
+    def path
+      @defaults['path']
+    end
+
+    def linkedin
+      @defaults['linkedin']
+    end
+
+    def frankies_flavours
+      %w[lemonade ginger-beer creme-soda cola]
+    end
+
+    def tokens
+      {}.tap do |h|
+        h[:jumio] = @defaults['jumio']['token'],
+        h[:school_api] = @defaults['school_api']['token'],
+        h[:cology] = @defaults['cology']['token'],
+        h[:idv] = @defaults['idv']['token'],
+        h[:comply_advantage] = @defaults['comply_advantage']['api_token'],
+        h[:credit_comm] = @defaults['credit_comm']['token'],
+        h[:zendesk] = @defaults['zendesk']['token']
+      end
+    end
+
+    def echosign
+      @defaults['echosign']
+    end
+
+    def recaptcha
+      @defaults['recaptcha']
+    end
+
     def error_condition
       @defaults['items']
     end
