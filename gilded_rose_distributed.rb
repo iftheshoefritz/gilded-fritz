659d877214d699c1065d3b48fbe9bdaaecc95fca
Fritz Meissner <fritz.meissner@gmail.com>
2018-04-17 14:05:43 +0200

    Add excellent UX

--- gilded_rose_distributed.rb
+++ gilded_rose_distributed.rb
@@ -8,16 +8,25 @@ store = GildedRose.new(
 day = 0
 
 puts "press i for current inventory, n to advance day"
+puts ""
+print "> "
 input = gets
 while(true)
   if input.strip == 'i'
-    puts "*** ITEMS DAY #{day}****"
+    puts ""
+    puts "*** ITEMS DAY #{day} ***"
     puts store.items.map {|item| "NAME: #{item.name} SELL_IN: #{item.sell_in} QUALITY: #{item.quality}"}
+    puts ""
   elsif input.strip == 'n'
     day += 1
     store.update_quality
+    puts ""
     puts "Advanced day to #{day}"
+    puts ""
   end
+  puts ""
   puts "press i for current inventory, n to advance day"
+  puts ""
+  print "> "
   input = gets
 end
